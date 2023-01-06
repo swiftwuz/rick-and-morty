848ac225191c24e90625a3512760afa37ee8bb90
Nana Kojo Ewusie <enanakojo@icloud.com>
2023-01-06 00:54:46 +0000

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Nana Kojo Ewusie on 06/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
