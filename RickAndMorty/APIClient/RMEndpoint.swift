743a8e5f0d5104acc881bc13877157ebd357d972
Nana Kojo Ewusie <enanakojo@icloud.com>
2023-01-07 20:40:50 +0000

    add docstrings and API related files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,15 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Nana Kojo Ewusie on 07/01/2023.
+//
+
+import Foundation
+
+/// Represents unique API endpoints
+@frozen enum RMEndpoint: String {
+    case character
+    case episode
+    case location
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Nana Kojo Ewusie on 07/01/2023.
+//
+
+import Foundation
+
+/// Object that represents an API call
+final class RMRequest {
+    
+}
